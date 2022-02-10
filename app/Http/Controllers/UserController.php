c4deab0b8b3ff14d2ca304a5aed533acdb10ef50
Ajay Suman <ajay.rewaatechverge@gmail.com>
2022-02-10 15:32:58 +0530

    Status Change

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -94,6 +94,7 @@ class UserController extends Controller {
                 }
             } catch ( \Exception $e ) {
                 return response()->json( [
+                    'status' => 422,
                     'message' => $e->getMessage()
                 ] );
 
